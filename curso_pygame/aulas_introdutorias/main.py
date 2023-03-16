99590eac651b188875d178d0e66f98e1b9f18813
Lavínia Amorim <lavinia.santos2105@gmail.com>
2023-03-16 15:36:32 -0300

    moving objects

--- curso_pygame/aulas_introdutorias/main.py
+++ curso_pygame/aulas_introdutorias/main.py
@@ -6,16 +6,24 @@ pygame.init()
 
 largura = 640
 altura = 480
+x = largura/2
+y = 0
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Game")
+relogio = pygame.time.Clock()
 
 while True:
+    relogio.tick(10)
+    tela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    pygame.draw.rect(tela, (255,0,0), (200,300,40,50))
-    pygame.draw.circle(tela, (0,0,255), (300,260), 40)
-    pygame.draw.line(tela, (255,255,0), (390,0), (390,600), 5)
+
+    pygame.draw.rect(tela, (255,0,0), (x,y,40,50))
+    if y >= altura:
+        y = 0
+    y += 1
+
     pygame.display.update()
