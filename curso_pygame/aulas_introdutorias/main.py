e24729536510d7c49cb09f1de902f6c63dc6ba75
Lavínia Amorim <lavinia.santos2105@gmail.com>
2023-03-16 15:24:15 -0300

    drawing objects

--- curso_pygame/aulas_introdutorias/main.py
+++ curso_pygame/aulas_introdutorias/main.py
@@ -12,7 +12,10 @@ pygame.display.set_caption("Game")
 
 while True:
     for event in pygame.event.get():
-        if event.type ==    QUIT:
+        if event.type == QUIT:
             pygame.quit()
             exit()
+    pygame.draw.rect(tela, (255,0,0), (200,300,40,50))
+    pygame.draw.circle(tela, (0,0,255), (300,260), 40)
+    pygame.draw.line(tela, (255,255,0), (390,0), (390,600), 5)
     pygame.display.update()
