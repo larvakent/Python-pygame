bf865d7b7d50e3cae238111baca62341a75a0dc5
Lavínia Amorim <lavinia.santos2105@gmail.com>
2023-03-17 15:22:20 -0300

    Snake Game part 2

--- curso_pygame/snake_game/main.py
+++ curso_pygame/snake_game/main.py
@@ -16,6 +16,10 @@ altura = 480
 x_cobra = int(largura/2)
 y_cobra = int(altura/2)
 
+velocidade = 10
+x_controle = velocidade
+y_controle = 0
+
 x_maca = randint(40, 600)
 y_maca = randint(50, 430)
 
@@ -27,7 +31,7 @@ pygame.display.set_caption("Game")
 relogio = pygame.time.Clock()
 
 lista_cobra = []
-
+comprimento_inicial = 5
 
 def aumenta_cobra(lista_cobra):
     for XeY in lista_cobra:
@@ -39,44 +43,62 @@ def aumenta_cobra(lista_cobra):
 while True:
     relogio.tick(30)
     tela.fill((255,255,255))
+
     mensagem = f"Pontos: {pontos}"
     texto_formatado = fonte.render(mensagem, True, (0,0,0))
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-        # if event.type == KEYDOWN:
-        #     if event.key == K_a:
-        #         x-=20
-        #     if event.key == K_d:
-        #         x+=20
-        #     if event.key == K_w:
-        #         y-=20
-        #     if event.key == K_s:
-        #         y+=20
 
-    if pygame.key.get_pressed()[K_a]:
-        x_cobra-=20  
-    if pygame.key.get_pressed()[K_d]:
-        x_cobra+=20    
-    if pygame.key.get_pressed()[K_w]:
-        y_cobra-=20  
-    if pygame.key.get_pressed()[K_s]:
-        y_cobra+=20  
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                if x_controle == velocidade:
+                    pass
+                else:   
+                    x_controle = -velocidade
+                    y_controle = 0
+            if event.key == K_d:
+                if x_controle == -velocidade:
+                    pass
+                else:
+                    x_controle = velocidade
+                    y_controle = 0
+            if event.key == K_w:
+                if y_controle == velocidade:
+                    pass
+                else:
+                    y_controle = -velocidade
+                    x_controle = 0
+            if event.key == K_s:
+                if y_controle == -velocidade:
+                    pass
+                else:
+                    y_controle  = velocidade
+                    x_controle = 0
     
+    x_cobra += x_controle
+    y_cobra += y_controle
+
     cobra = pygame.draw.rect(tela, (0,255,0), (x_cobra,y_cobra,20,20))
     maca = pygame.draw.rect(tela, (255,0,0), (x_maca,y_maca,20,20))
 
     if cobra.colliderect(maca):
-        x_maca = randint(40, 600)
-        x_maca = randint(50, 430)
+        x_maca = randint(20, 600)
+        x_maca = randint(30, 430)
         pontos+=1
         barulho_colisao.play()
+        comprimento_inicial+=1
 
     lista_cabeca = []
     lista_cabeca.append(x_cobra)
     lista_cabeca.append(y_cobra)
+
     lista_cobra.append(lista_cabeca)
+
+    if len(lista_cobra) > comprimento_inicial:
+        del lista_cobra[0]
 
     aumenta_cobra(lista_cobra)
 
