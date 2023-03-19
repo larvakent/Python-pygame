3be89510971daeba65532b8946fa216ad01fe6cb
Lavínia Amorim <lavinia.santos2105@gmail.com>
2023-03-19 15:27:30 -0300

    criando sprites com piskel

--- curso_pygame/sprites/main.py
+++ curso_pygame/sprites/main.py
@@ -12,45 +12,33 @@ PRETO = (0,0,0)
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Sprites")
 
-class Sapo(pygame.sprite.Sprite):
+class Personagem(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.sprites = []
-        self.sprites.append(pygame.image.load("attack_1.png"))
-        self.sprites.append(pygame.image.load("attack_2.png"))
-        self.sprites.append(pygame.image.load("attack_3.png"))
-        self.sprites.append(pygame.image.load("attack_4.png"))
-        self.sprites.append(pygame.image.load("attack_5.png"))
-        self.sprites.append(pygame.image.load("attack_6.png"))
-        self.sprites.append(pygame.image.load("attack_7.png"))
-        self.sprites.append(pygame.image.load("attack_8.png"))
-        self.sprites.append(pygame.image.load("attack_9.png"))
-        self.sprites.append(pygame.image.load("attack_10.png"))
+        self.sprites.append(pygame.image.load("homem-terno-anim/sprite_0.png"))
+        self.sprites.append(pygame.image.load("homem-terno-anim/sprite_1.png"))
+        self.sprites.append(pygame.image.load("homem-terno-anim/sprite_2.png"))
         self.atual = 0
         self.image = self.sprites[self.atual]
-        self.image= pygame.transform.scale(self.image, (128*3, 64*3))
+        self.image= pygame.transform.scale(self.image, (64*5, 64*5))
 
         self.rect = self.image.get_rect()
-        self.rect.topleft = 100, 100
-
-        self.animar = False
-
-    def atacar(self):
-            self.animar = True
+        self.rect.topleft = 300, 250
 
     def update(self):
-        if self.animar == True:
-            self.atual += 0.4
-            if self.atual >= len(self.sprites):
-                self.atual = 0
-                self.animar = False
-            self.image = self.sprites[int(self.atual)]
-            self.image= pygame.transform.scale(self.image, (128*3, 64*3))
+        self.atual += 0.4
+        if self.atual >= len(self.sprites):
+            self.atual = 0
+        self.image = self.sprites[int(self.atual)]
+        self.image= pygame.transform.scale(self.image, (64*5, 6*54))
 
-    
 todas_as_sprites = pygame.sprite.Group()
-sapo = Sapo()
-todas_as_sprites.add(sapo)
+personagem = Personagem()
+todas_as_sprites.add(personagem)
+
+imagem_fundo = pygame.image.load("homem-terno-anim/cidade_fundo.jpg").convert()
+imagem_fundo = pygame.transform.scale(imagem_fundo, (largura, altura))
 
 relogio = pygame.time.Clock()
 
@@ -61,9 +49,8 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
-        if event.type == KEYDOWN:
-            sapo.atacar()
 
+    tela.blit(imagem_fundo, (0,0))
     todas_as_sprites.draw(tela)
     todas_as_sprites.update()
     pygame.display.flip()
