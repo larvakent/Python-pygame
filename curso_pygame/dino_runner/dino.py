e95930bcb778faab24d9c2d9bb75b8d6ade8d983
Lavínia Amorim <lavinia.santos2105@gmail.com>
2023-03-20 19:49:12 -0300

    Dino Runner part 3

--- curso_pygame/dino_runner/dino.py
+++ curso_pygame/dino_runner/dino.py
@@ -22,6 +22,10 @@ pygame.display.set_caption("Dino Runner")
 
 sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, "dinoSpritesheet.png")).convert_alpha()
 
+som_colisao = pygame.mixer.Sound(os.path.join(diretorio_sons, "sons_death_sound.wav"))
+som_colisao.set_volume(1)
+colidiu = False
+
 class Dino(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -36,6 +40,7 @@ class Dino(pygame.sprite.Sprite):
         self.index_lista = 0
         self.image = self.imagens_dino[self.index_lista]
         self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
         self.pos_y_inicial = ALTURA - 64 - 96//2
         self.rect.center = [100, ALTURA - 64]
         self.pulo = False
@@ -89,6 +94,19 @@ class Chao(pygame.sprite.Sprite):
             self.rect.x = LARGURA 
         self.rect.x -= 10
 
+class Cacto(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((5*32, 0), (32, 32))
+        self.image = pygame.transform.scale(self.image, (32*2, 32*2))
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.center = (LARGURA, ALTURA - 64)
+
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = LARGURA
+        self.rect.x -= 10
 
 todas_as_sprites = pygame.sprite.Group()
 dino = Dino()
@@ -101,6 +119,12 @@ for i in range(3):
 for i in range(20):
     chao = Chao(i)
     todas_as_sprites.add(chao)
+
+cacto = Cacto()
+todas_as_sprites.add(cacto)
+
+grupo_obstaculos = pygame.sprite.Group()
+grupo_obstaculos.add(cacto)
 
 relogio = pygame.time.Clock()
 while True:
@@ -117,7 +141,16 @@ while True:
                 else:
                     dino.pular()
 
+    colisoes = pygame.sprite.spritecollide(dino, grupo_obstaculos, False, pygame.sprite.collide_mask)
+    
     todas_as_sprites.draw(tela)
-    todas_as_sprites.update()
+    
+    if colisoes and colidiu == False:
+        som_colisao.play()
+        colidiu = True
+    if colidiu == True:
+        pass
+    else:
+        todas_as_sprites.update()
 
     pygame.display.flip()
