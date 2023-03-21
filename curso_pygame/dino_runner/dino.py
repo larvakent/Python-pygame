c569eeecddd171f120913505c9da51a38b4efa2e
Lavínia Amorim <lavinia.santos2105@gmail.com>
2023-03-21 08:46:56 -0300

    Dino Runner Final

--- curso_pygame/dino_runner/dino.py
+++ curso_pygame/dino_runner/dino.py
@@ -43,6 +43,14 @@ def exibe_mensagem(msg, tamanho, cor):
     texto_formatado = fonte.render(mensagem, True, cor)
     return texto_formatado
 
+def reiniciar_jogo():
+    pontos = 0
+    velocidade_jogo = 10
+    colidiu = False
+    dino_voador.rect.x = LARGURA
+    cacto.rect.x = LARGURA
+    escolha_obstaculo = choice([0,1])
+
 class Dino(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -192,6 +200,9 @@ while True:
                     pass
                 else:
                     dino.pular()
+            if event.key == K_r and colidiu == True:
+                reiniciar_jogo()
+             
 
     colisoes = pygame.sprite.spritecollide(dino, grupo_obstaculos, False, pygame.sprite.collide_mask)
     
@@ -211,7 +222,10 @@ while True:
     if colidiu == True:
         if pontos % 100 == 0:
             pontos += 1
-        pass
+        game_over = exibe_mensagem("GAME OVER", 40, (0,0,0))
+        tela.blit(game_over, (LARGURA//2, ALTURA//2))
+        restart = exibe_mensagem("Pressione R par reiniciar", 20, (0,0,0))
+        tela.blit(restart, (LARGURA//2,(ALTURA//2)+60))
     else:
         pontos += 1
         todas_as_sprites.update()
